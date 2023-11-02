7214c50f56aec1468bc472942377160b0468648a
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-02 10:18:55 +0545

    feat: add svelte components

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,21 @@ import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { astroImageTools } from 'astro-imagetools';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
-  site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  trailingSlash: 'always', // Use to always append '/' at end of url
+  site: 'https://example.com/',
+  // Use to generate your sitemap and canonical URLs in your final build.
+  trailingSlash: 'always',
+  // Use to always append '/' at end of url
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'monokai',
-    },
+      theme: 'monokai'
+    }
   },
-  integrations: [
-    tailwind({}),
-    sitemap(),
-    robotsTxt(),
-    astroImageTools,
-  ],
+  integrations: [tailwind({}), sitemap(), robotsTxt(), astroImageTools, svelte()]
 });
