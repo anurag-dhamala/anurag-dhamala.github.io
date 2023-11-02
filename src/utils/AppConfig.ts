c12fc060e8ac31cac3cfeb9d47a274005cdda3a9
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-02 15:12:07 +0545

    feat: add config

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+    site_name: 'Anurag',
+    title: 'Anurag',
+    description: 'Portfolio of Anurag Dhamala. Software Engineer',
+    author: 'Anurag Dhamala',
+    locale_region: 'en-us',
+    locale: 'en',
+  };
