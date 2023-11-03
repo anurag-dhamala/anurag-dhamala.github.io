a47cbfeb5add10e4445014920437b8b7c78d1093
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-03 13:50:01 +0545

    feat: add projects, links and static data

--- src/utils/static-data.ts
+++ src/utils/static-data.ts
@@ -2,18 +2,27 @@ export const projects = [
     {
         title: "go-task-cli",
         description: "simple todo list cli in go",
-        date: ""
+        date: "",
+        link: "https://github.com/anurag-dhamala/go-task-cli"
+    },
+    {
+        title: "portfolio",
+        description: "simple portfolio built using Astro",
+        date: "",
+        link: "https://anurag-dhamala.github.io"
     },
     {
         title: "Let's Read Asia",
-        description: "",
-        date: "Jan, 2020"
+        description: "books reading platform for children all around Asia",
+        date: "Jan, 2020",
+        link: "https://letsreadasia.org"
     },
 
     {
         title: "Hamro Chautari",
         description: "Video calling platform using livekit",
-        date: ""
+        date: "",
+        link: "https://chautari.hamropatro.com"
     }
 ]
 
