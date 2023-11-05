4c80de3f63b4d4cef636ebcbce7b36fbc93cc79f
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-05 11:12:49 +0545

    fix: remove trailing slash
    desc: looks like routing won't work with trailing slash: always

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,11 +10,11 @@ export default defineConfig({
   base: '.', // Set a path prefix.
   site: 'https://anurag-dhamala.github.io/',
   // Use to generate your sitemap and canonical URLs in your final build.
-  trailingSlash: 'always',
+  // trailingSlash: 'always',
   // Use to always append '/' at end of url
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
+      // Choose from Shiki's built-in themes (or add your own),
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'monokai'
     }
