b1d30bd3021e323a9112fe1799422605d8571e82
Anurag Dhamala <anuragdhamala8@gmail.com>
2023-11-07 06:13:01 +0545

    fix: change page title and description acc. to the page info

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-    site_name: 'Anurag',
-    title: 'Anurag',
+    site_name: 'Anurag Dhamala',
+    title: 'Anurag D.',
     description: 'Portfolio of Anurag Dhamala. Software Engineer',
     author: 'Anurag Dhamala',
     locale_region: 'en-us',
